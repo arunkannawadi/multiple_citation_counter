1a316f9e98f67722043701e8099786a93ac64df3
arunkannawadi <kj.arun.kj@gmail.com>
2022-06-21 20:59:11 -0400

    Show a form

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 import time
  
 app = Flask(__name__)
  
-@app.route("/")
+@app.route("/timer")
 def home_view():
     def inner():
         for x in range(100):
             time.sleep(1)
             yield '%s<br/>\n' % x
     return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show the partial page immediately
+
+@app.route('/')
+def home():
+    return render_template('home.html')
